d0193f4eded50c0d5a1f3575c43dca6052469f2e
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-12 14:11:21 -0700

    Initial commit

---
+++ loops/count_down/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	for i := 10; i > 0; i-- {
+		//Printf %v means to print the value of i
+		fmt.Printf("%v \n", i)
+		//wait half a second
+		time.Sleep(time.Second / 2)
+	}
+
+	fmt.Println("BOOM!")
+}
