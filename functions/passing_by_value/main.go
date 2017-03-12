bc42369c73c6076c13e366a3db0e4a4624e42764
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-12 15:40:16 -0700

    Basic passing by value

---
+++ functions/passing_by_value/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func take_value(i int) {
+	fmt.Println("In take_value function. i is:", i, "Located at:", &i)
+	i = 0
+	fmt.Println("In take_value function. i is reset and value of i is:", i, "located at:", &i, "\n")
+}
+
+func reset_value(i int) int {
+	fmt.Print("In reset_value function. i comes in as: ", i, " Located at:", &i, "\n")
+	i = 0
+	fmt.Print("In reset_value function. i returns as: ", i, " Located at:", &i, "\n \n")
+	return i
+}
+
+func main() {
+	i := 15
+	fmt.Println("In main function. i is:", i, "Located at:", &i, "\n")
+	take_value(i)
+	fmt.Println("In main function after take_value. i is:", i, "Located at:", &i, "\n")
+	i = reset_value(i)
+	fmt.Println("In main function after reset_value takes i and replces mains i value. i is:", i, "Located at:", &i, "\n")
+
+}
