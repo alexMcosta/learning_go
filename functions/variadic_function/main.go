53539f3bf339c5c207842aade1077f650f8c4d32
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-13 20:05:51 -0700

    Add notes to Variadic Functions

--- functions/variadic_function/main.go
+++ functions/variadic_function/main.go
@@ -1,3 +1,15 @@
+/***************************************************************
+Variadic Function
+
+I AM SUPER EXCITED ABOUT THIS! Almost to the point where this might be a hammmer(Refrence to everything being a nail).
+
+Variadic functions seem (I will use seem about everything until I know it as a fact) that they are functions that can take more then one input by creating a slice and filling it by the given variable
+
+below we have a variadic function that states
+-------func add_grocery(a ...string){...}---------
+What this is saying is that when called create a slice or array named 'a' where each value will be a string. even if there is one value it comes in as an array or slice.
+
+***************************************************************/
 package main
 
 import "fmt"
