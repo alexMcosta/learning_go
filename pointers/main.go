6bf781ca25f4ad8bde60f88e4f97828c3c3f9507
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-14 18:05:08 -0700

    Add pointers

---
+++ pointers/main.go
@@ -0,0 +1,24 @@
+/**************************************************************
+Pointer
+
+Pointers are variables pointing to a memory location
+
+**************************************************************/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 43
+
+	fmt.Println(a)
+	fmt.Println(&a)
+
+	// when the type has a '*' it means that the variables type is a pointer to said type
+	var b *int = &a
+
+	fmt.Println(b)
+
+}
