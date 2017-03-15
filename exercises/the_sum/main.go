44ab044c7eee4ed46a896dd30ccc718f78f51c80
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-14 22:20:32 -0700

    Complete The Sum Exercise

--- exercises/the_sum/main.go
+++ exercises/the_sum/main.go
@@ -4,24 +4,13 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Give me a small number and a larger number:")
+	var sum int
 
-	var a, b, remainder, bigger, smaller int
+	for i := 0; i <= 1000; i++ {
 
-	fmt.Scan(&a, &b)
-
-	if a > b {
-		remainder = a % b
-		bigger = a
-		smaller = b
-	} else if b > a {
-		remainder = b % a
-		bigger = b
-		smaller = a
-	} else {
-		fmt.Println("Those are the same number!")
-		return
+		if i%3 == 0 || i%5 == 0 {
+			sum = sum + i
+		}
 	}
-
-	fmt.Println("The remainder of ", bigger, " devided by ", smaller, " is ", remainder)
+	fmt.Println(sum)
 }
