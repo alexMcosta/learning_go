4e726faa0a67741f323838ff4e66f61ab2a072e3
Alex Costa <alexmcosta.dev@gmail.com>
2017-03-15 19:15:13 -0700

    Add callback

---
+++ functions/callbacks/main.go
@@ -0,0 +1,22 @@
+// Callbacks: passing a function through a function
+package main
+
+import "fmt"
+
+func visit(numbers []int, callback func(int)) {
+	for _, n := range numbers {
+		callback(n)
+	}
+}
+
+func add_two(n int) {
+	fmt.Println(n + 2)
+}
+
+func main() {
+	visit([]int{1, 2, 3, 4}, func(n int) {
+		fmt.Println(n)
+	})
+	//NO WORK
+	//visit([]int{1, 2, 3, 4}, add_two(n int))
+}
