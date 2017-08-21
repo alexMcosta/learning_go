c027b47f7937ca18dbb16369abaaa8b93f8d1377
Alex Costa <alexmcosta.dev@gmail.com>
2017-08-21 09:50:06 -0700

    Add Load pages method

--- projects/gowiki/wiki.go
+++ projects/gowiki/wiki.go
@@ -15,3 +15,10 @@ func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
+
+// Load pages
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
