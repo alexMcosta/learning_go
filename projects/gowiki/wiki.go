0c2ef5270ad3e951c212a5615544b522791844c8
Alex Costa <alexmcosta.dev@gmail.com>
2017-08-21 10:01:53 -0700

    fix error handling for loadPage

--- projects/gowiki/wiki.go
+++ projects/gowiki/wiki.go
@@ -17,11 +17,19 @@ func (p *Page) save() error {
 }
 
 // Load pages
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error){
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+//Main
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
