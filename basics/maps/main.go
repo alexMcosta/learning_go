2b04799f6c96cc42957e19d1d05ba6e9aa9e81b0
alexandercosta <alexander.costa@gravitatedesign.com>
2017-11-02 16:50:56 -0700

    Add nested maps example

--- basics/maps/main.go
+++ basics/maps/main.go
@@ -13,8 +13,8 @@ func main() {
 	myGreeting["Alex"] = "hello"
 	myGreeting["Corey"] = "howdey"
 
-	fmt.Println(myGreeting)
-	fmt.Println(myGreeting["Alex"])
+	//fmt.Println(myGreeting)
+	//fmt.Println(myGreeting["Alex"])
 
 	//Using a For Range with a map
 	for v, k := range myGreeting {
@@ -23,5 +23,39 @@ func main() {
 
 	// Deleteing from a map is like so:
 	delete(myGreeting, "Corey")
-	fmt.Println(myGreeting)
+	//fmt.Println(myGreeting)
+
+	//Nested Maps
+	myFamily := map[string]map[string]int{
+		"Nat": map[string]int{
+			"Daughters": 2,
+			"Sons":      0,
+			"Pets":      1,
+		},
+		"Ashley": map[string]int{
+			"Daughters": 0,
+			"Sons":      1,
+			"Pets":      2,
+		},
+		"Holishia": map[string]int{
+			"Daughters": 0,
+			"Sons":      1,
+			"Pets":      12,
+		},
+		"Teddy": map[string]int{
+			"Daughters": 1,
+			"Sons":      0,
+			"Pets":      0,
+		},
+		"Kevin": map[string]int{
+			"Daughters": 0,
+			"Sons":      1,
+			"Pets":      0,
+		},
+	}
+
+	fmt.Println(myFamily)
+	fmt.Println(myFamily["Teddy"])
+	fmt.Println(myFamily["Teddy"]["Sons"])
+
 }
