40d143d9718750bfdb87a7aab3da2f464dd670aa
Alex Costa <alexmcosta.dev@gmail.com>
2017-11-01 21:53:18 -0700

    Add notes to Map section

--- basics/maps/main.go
+++ basics/maps/main.go
@@ -1,12 +1,27 @@
+/**********************************************************
+Maps are key vlue refrence types
+
+**********************************************************/
+
 package main
 
 import "fmt"
 
 func main() {
-	myGreeting := map[string]string{}
+	myGreeting := map[string]string{} //Initiate a blank map
 
 	myGreeting["Alex"] = "hello"
 	myGreeting["Corey"] = "howdey"
 
+	fmt.Println(myGreeting)
+	fmt.Println(myGreeting["Alex"])
+
+	//Using a For Range with a map
+	for v, k := range myGreeting {
+		fmt.Println(v, k)
+	}
+
+	// Deleteing from a map is like so:
+	delete(myGreeting, "Corey")
 	fmt.Println(myGreeting)
 }
