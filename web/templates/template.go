ba7e9d0d8a9bce7bcf4baf7758888a31ccceb935
alexMcosta <alexmcosta.dev@gmail.com>
2017-11-08 17:57:12 -0800

    Add notes about template.ParseGlob

--- web/templates/template.go
+++ web/templates/template.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	//Parse a single template if you want to parse more then one
+	//Use template.ParseGlob("location/*")
 	tpl, err := template.ParseFiles("tpl.gohtml")
 	if err != nil {
 		log.Fatalln(err)
