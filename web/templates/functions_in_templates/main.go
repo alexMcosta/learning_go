12b0e291da40918bb74cd734740a0928ef753b71
alexMcosta <alexmcosta.dev@gmail.com>
2017-11-09 15:02:22 -0800

    Took the creation of tpl out of its own random function

--- web/templates/functions_in_templates/main.go
+++ web/templates/functions_in_templates/main.go
@@ -9,19 +9,20 @@ import (
 
 var tpl *template.Template
 
-func init() {
-	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
-}
-
 func monthDayYear(t time.Time) string {
 	return t.Format("01-02-2006")
 }
 
+//To pass a function through a template you need FuncMap
+//It Is a composite literal
+//It is basically a map for functions
 var fm = template.FuncMap{
 	"fdateMDY": monthDayYear,
 }
 
 func main() {
+
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
 
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
 	if err != nil {
