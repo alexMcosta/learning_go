d46a639299f996c29773a834ad4327d19c21c131
alexMcosta <alexmcosta.dev@gmail.com>
2017-11-26 00:52:37 -0800

    Finish section 6.1

---
+++ web/book_tuts/usegolang.com/chap_6/lenslocked.com/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var homeTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, e *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to"+"<a href=\"mailto:support@lenslocked.com\">"+"support@lenslocked.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>This is the FAQ page!</h1>")
+}
+
+func catchAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>No Page exists</h1>")
+}
+
+func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	var h http.Handler = http.HandlerFunc(catchAll)
+	r := mux.NewRouter()
+	r.NotFoundHandler = h
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	http.ListenAndServe(":3000", r)
+}
