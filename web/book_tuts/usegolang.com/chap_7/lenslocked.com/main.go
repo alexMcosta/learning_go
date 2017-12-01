280663d67da5acf60ae0679fd401a0c1f6795a17
alexMcosta <alexmcosta.dev@gmail.com>
2017-12-01 12:40:39 -0800

    Edit main.go to work with signup

--- web/book_tuts/usegolang.com/chap_7/lenslocked.com/main.go
+++ web/book_tuts/usegolang.com/chap_7/lenslocked.com/main.go
@@ -9,10 +9,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-var faqView *views.View
-var catchAllView *views.View
+var (
+	homeView     *views.View
+	contactView  *views.View
+	faqView      *views.View
+	catchAllView *views.View
+	signupView   *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -27,6 +30,11 @@ func contact(w http.ResponseWriter, e *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
 }
 
 func catchAll(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +54,7 @@ func main() {
 	contactView = views.NewView("bootstap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
 	catchAllView = views.NewView("bootstrap", "views/catchall.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	var h http.Handler = http.HandlerFunc(catchAll)
 	r := mux.NewRouter()
@@ -53,5 +62,6 @@ func main() {
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
