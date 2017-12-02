11c6c48cf5d7b16bf3a0ced16fb06570ae3e6a09
alexMcosta <alexmcosta.dev@gmail.com>
2017-12-02 07:25:34 -0800

    Create New handler

--- web/book_tuts/usegolang.com/chap_7/lenslocked.com/controllers/users.go
+++ web/book_tuts/usegolang.com/chap_7/lenslocked.com/controllers/users.go
@@ -1,7 +1,23 @@
 package controllers
 
-import "github.com/alexMcosta/learning_go/web/book_tuts/usegolang.com/chap_6/lenslocked.com/views"
+import (
+	"net/http"
+
+	"github.com/alexMcosta/learning_go/web/book_tuts/usegolang.com/chap_6/lenslocked.com/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
 
 type Users struct {
 	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
