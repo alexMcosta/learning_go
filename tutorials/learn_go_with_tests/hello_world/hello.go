ec99648b53e8362f456138ae1383478fa5cc93c2
acosta <alexmcosta.dev@gmail.com>
2019-04-13 23:56:22 -0700

    Make spanish lang pass

--- tutorials/learn_go_with_tests/hello_world/hello.go
+++ tutorials/learn_go_with_tests/hello_world/hello.go
@@ -5,12 +5,17 @@ import "fmt"
 const englishPrefix = "Hello, "
 
 //Hello ... Go lint won't stop riding me about adding a comment here
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishPrefix + name
 }
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- tutorials/learn_go_with_tests/hello_world/hello_test.go
+++ tutorials/learn_go_with_tests/hello_world/hello_test.go
@@ -12,15 +12,22 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Alex", "Spanish")
+		want := "Hola, Alex"
 
 		assertCorrectMessage(t, got, want)
 	})
