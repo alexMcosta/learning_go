e4be7084c39f28d03a27b24b6a72125e19f9c627
acosta <alexmcosta.dev@gmail.com>
2019-04-28 14:21:44 -0700

    Going over the example again:

--- tutorials/learn_go_with_tests/hello_world/hello.go
+++ tutorials/learn_go_with_tests/hello_world/hello.go
@@ -2,30 +2,10 @@ package main
 
 import "fmt"
 
-const englishPrefix = "Hello, "
-const spanishPrefix = "Hola, "
-const frenchPrefix = "Bonjour, "
-const spanish = "Spanish"
-const french = "French"
-
-//Hello ... Go lint won't stop riding me about adding a comment here
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := englishPrefix
-
-	switch language {
-	case french:
-		prefix = frenchPrefix
-	case spanish:
-		prefix = spanishPrefix
-	}
-
-	return prefix + name
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello("World", ""))
+	fmt.Println(Hello("World"))
 }
--- tutorials/learn_go_with_tests/hello_world/hello_test.go
+++ tutorials/learn_go_with_tests/hello_world/hello_test.go
@@ -3,40 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
-	assertCorrectMessage := func(t *testing.T, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got `%s` want `%s`", got, want)
-		}
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
-
-	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Chris", "")
-		want := "Hello, Chris"
-
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("", "")
-		want := "Hello, World"
-
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("In Spanish", func(t *testing.T) {
-		got := Hello("Alex", "Spanish")
-		want := "Hola, Alex"
-
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("In French", func(t *testing.T) {
-		got := Hello("Alex", "French")
-		want := "Bonjour, Alex"
-
-		assertCorrectMessage(t, got, want)
-	})
-
 }
