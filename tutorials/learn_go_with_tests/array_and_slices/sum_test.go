7296abed4bc3487b2bc992034567069e0dd6c46a
acosta <alexmcosta.dev@gmail.com>
2019-04-29 08:00:06 -0700

    Left off at end of Array and Slices

--- tutorials/learn_go_with_tests/array_and_slices/sum_test.go
+++ tutorials/learn_go_with_tests/array_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3}
@@ -10,5 +13,23 @@ func TestSum(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{0, 9})
+	want := []int{6, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
