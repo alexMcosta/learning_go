c07526cd5e6eeb5fa2e05812651f278b49863c16
acosta <alexmcosta.dev@gmail.com>
2019-04-29 06:52:57 -0700

    Repeater with now be able to take an int

--- tutorials/learn_go_with_tests/iteration/repeat.go
+++ tutorials/learn_go_with_tests/iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(char string) string {
+func Repeat(char string, x int) string {
 	var output string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < x; i++ {
 		output += char
 	}
 	return output
