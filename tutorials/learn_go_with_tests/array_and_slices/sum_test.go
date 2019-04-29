ec90af1404bc3a1a800381a6f5d30eedf70a2a5c
acosta <alexmcosta.dev@gmail.com>
2019-04-29 07:22:36 -0700

    Learned go test -cover

--- tutorials/learn_go_with_tests/array_and_slices/sum_test.go
+++ tutorials/learn_go_with_tests/array_and_slices/sum_test.go
@@ -3,26 +3,12 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3}
 
-	t.Run("Collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	want := 6
 
-		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("Got %d want %d given, %v", got, want, numbers)
-		}
-	})
-
-	t.Run("Collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
 }
