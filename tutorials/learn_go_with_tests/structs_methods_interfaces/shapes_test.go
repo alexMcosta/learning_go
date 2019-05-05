9f92e069564834226b578787d17d050fa3747757
acosta <alexmcosta.dev@gmail.com>
2019-05-05 15:38:03 -0700

    Make triangle test pass

--- tutorials/learn_go_with_tests/structs_methods_interfaces/shapes_test.go
+++ tutorials/learn_go_with_tests/structs_methods_interfaces/shapes_test.go
@@ -13,11 +13,20 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.00, 6.00}
-	got := Area(rectangle)
-	want := 72.00
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
 	}
 }
