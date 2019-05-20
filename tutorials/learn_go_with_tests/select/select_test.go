1413f662fc7121457637fcc9fb993d53098ef79f
acosta <alexmcosta.dev@gmail.com>
2019-05-20 15:21:04 -0700

    Refactor select chapter

--- tutorials/learn_go_with_tests/select/select_test.go
+++ tutorials/learn_go_with_tests/select/select_test.go
@@ -1,10 +1,20 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	slowS := makeDelayedServer(20 * time.Millisecond)
+	fastS := makeDelayedServer(20 * time.Millisecond)
+	defer slowS.Close()
+	defer fastS.Close()
+
+	slowURL := slowS.URL
+	fastURL := fastS.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +22,11 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
